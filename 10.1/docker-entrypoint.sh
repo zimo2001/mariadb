1fa1e597810785e005b9de8cbf0f7daee7ac2e99
Motti Haim <mottih@walla.net.il>
2016-02-20 00:15:05 +0200

    docker-entrypoint.sh for galera

--- 10.1/docker-entrypoint.sh
+++ 10.1/docker-entrypoint.sh
@@ -1,108 +1,110 @@
 #!/bin/bash
-set -eo pipefail
+set -e
 
-# if command starts with an option, prepend mysqld
 if [ "${1:0:1}" = '-' ]; then
-	set -- mysqld "$@"
+    set -- mysqld "$@"
 fi
 
 if [ "$1" = 'mysqld' ]; then
-	# Get config
-	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+    # read DATADIR from the MySQL config
+    DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+    
+    if [ ! -d "$DATADIR/mysql" ]; then
+        if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
+            echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
+            echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
+            exit 1
+        fi
 
-	if [ ! -d "$DATADIR/mysql" ]; then
-		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
-			echo >&2 'error: database is uninitialized and password option is not specified '
-			echo >&2 '  You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
-			exit 1
-		fi
+        echo 'Running mysql_install_db ...'
+                mysql_install_db --datadir="$DATADIR"
+                echo 'Finished mysql_install_db'
 
-		mkdir -p "$DATADIR"
-		chown -R mysql:mysql "$DATADIR"
+        
+        # These statements _must_ be on individual lines, and _must_ end with
+        # semicolons (no line breaks or comments are permitted).
+        # TODO proper SQL escaping on ALL the things D:
+        
+        tempSqlFile='/tmp/mysql-first-time.sql'
+        cat > "$tempSqlFile" <<-EOSQL
+DELETE FROM mysql.user ;
+CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+DROP DATABASE IF EXISTS test ;
+EOSQL
+        
+        if [ "$MYSQL_DATABASE" ]; then
+            echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
+        fi
+        
+        if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+            echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
+            
+            if [ "$MYSQL_DATABASE" ]; then
+                echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
+            fi
+        fi
 
-		echo 'Initializing database'
-		mysql_install_db --user=mysql --datadir="$DATADIR" --rpm
-		echo 'Database initialized'
+        if [ -n "$GALERA_CLUSTER" -a "$GALERA_CLUSTER" = true ]; then
+            WSREP_SST_USER=${WSREP_SST_USER:-"sst"}
+            if [ -z "$WSREP_SST_PASSWORD" ]; then
+                echo >&2 'error: database is uninitialized and WSREP_SST_PASSWORD not set'
+                echo >&2 '  Did you forget to add -e WSREP_SST_PASSWORD=xxx ?'
+                exit 1
+            fi
 
-		"$@" --skip-networking &
-		pid="$!"
+            cp /tmp/cluster.cnf /etc/mysql/conf.d/cluster.cnf
 
-		mysql=( mysql --protocol=socket -uroot )
+            sed -i -e "s|wsrep_sst_auth \= \"sstuser:changethis\"|wsrep_sst_auth = ${WSREP_SST_USER}:${WSREP_SST_PASSWORD}|" /etc/mysql/conf.d/cluster.cnf
 
-		for i in {30..0}; do
-			if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
-				break
-			fi
-			echo 'MySQL init process in progress...'
-			sleep 1
-		done
-		if [ "$i" = 0 ]; then
-			echo >&2 'MySQL init process failed.'
-			exit 1
-		fi
+            WSREP_NODE_ADDRESS=`ip addr show | grep -E '^[ ]*inet' | grep -m1 global | awk '{ print $2 }' | sed -e 's/\/.*//'`
+            if [ -n "$WSREP_NODE_ADDRESS" ]; then
+                                sed -i -e "s|^#wsrep_node_address \= .*$|wsrep_node_address = ${WSREP_NODE_ADDRESS}|" /etc/mysql/conf.d/cluster.cnf
+                if [ -n "$WSREP_CLUSTER_ADDRESS"  -a "$WSREP_CLUSTER_ADDRESS" != "gcomm://" ]; then
+                    WSREP_CLUSTER_ADDRESS="${WSREP_CLUSTER_ADDRESS},${WSREP_NODE_ADDRESS}"
+                fi
+            fi
+ 	    # CoreOS, using fleet
+            if [ -n "$FLEETCTL_ENDPOINT" -a -e './etcdctl' -a -z "$WSREP_CLUSTER_ADDRESS" ]; then
+                WSREP_CLUSTER_ADDRESS=""
+                for key in $(./etcdctl --peers=${FLEETCTL_ENDPOINT} ls /galera/|| true); do
+                    WSREP_NODE=$(./etcdctl --peers=${FLEETCTL_ENDPOINT} get ${key} || true)
+                    if [ "$WSREP_CLUSTER_ADDRESS" != '' ]; then
+                        WSREP_CLUSTER_ADDRESS=$WSREP_CLUSTER_ADDRESS,${WSREP_NODE}
+                    else
+                        WSREP_CLUSTER_ADDRESS=${WSREP_NODE}
+                    fi
+                done
+                WSREP_CLUSTER_ADDRESS=gcomm://${WSREP_CLUSTER_ADDRESS}
+            fi
+            # Kubernetes
+	    # if kubernetes, take advantage of the metadata, unless of course already set
+            if [ -n "$KUBERNETES_RO_SERVICE_HOST" -a -e './kubectl' -a -z "$WSREP_CLUSTER_ADDRESS" ]; then
+                WSREP_CLUSTER_ADDRESS=gcomm://
+                for node in 1 2 3; do
+                    WSREP_NODE=`./kubectl --server=${KUBERNETES_RO_SERVICE_HOST}:${KUBERNETES_RO_SERVICE_PORT} get pods| grep "^pxc-node${node}" | tr -d '\n' | awk '{ print $2 }'`
+                    if [ ! -z $WSREP_NODE ]; then
+                        if [ $node -gt 1 -a $node != "" ]; then
+                            WSREP_NODE=",${WSREP_NODE}"
+                        fi
+                        WSREP_CLUSTER_ADDRESS="${WSREP_CLUSTER_ADDRESS}${WSREP_NODE}"
+                    fi
+                done
+            fi 
 
-		if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]; then
-			# sed is for https://bugs.mysql.com/bug.php?id=20545
-			mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
-		fi
+            if [ -n "$WSREP_CLUSTER_ADDRESS" -a "$WSREP_CLUSTER_ADDRESS" != "gcomm://" ]; then
+                sed -i -e "s|wsrep_cluster_address \= gcomm://|wsrep_cluster_address = ${WSREP_CLUSTER_ADDRESS}|" /etc/mysql/conf.d/cluster.cnf
+            fi
 
-		if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
-			MYSQL_ROOT_PASSWORD="$(pwgen -1 32)"
-			echo "GENERATED ROOT PASSWORD: $MYSQL_ROOT_PASSWORD"
-		fi
-		"${mysql[@]}" <<-EOSQL
-			-- What's done in this file shouldn't be replicated
-			--  or products like mysql-fabric won't work
-			SET @@SESSION.SQL_LOG_BIN=0;
-
-			DELETE FROM mysql.user ;
-			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-			DROP DATABASE IF EXISTS test ;
-			FLUSH PRIVILEGES ;
-		EOSQL
-
-		if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
-			mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
-		fi
-
-		if [ "$MYSQL_DATABASE" ]; then
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
-			mysql+=( "$MYSQL_DATABASE" )
-		fi
-
-		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
-
-			if [ "$MYSQL_DATABASE" ]; then
-				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
-			fi
-
-			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
-		fi
-
-		echo
-		for f in /docker-entrypoint-initdb.d/*; do
-			case "$f" in
-				*.sh)     echo "$0: running $f"; . "$f" ;;
-				*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
-				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
-				*)        echo "$0: ignoring $f" ;;
-			esac
-			echo
-		done
-
-		if ! kill -s TERM "$pid" || ! wait "$pid"; then
-			echo >&2 'MySQL init process failed.'
-			exit 1
-		fi
-
-		echo
-		echo 'MySQL init process done. Ready for start up.'
-		echo
-	fi
-
-	chown -R mysql:mysql "$DATADIR"
+            echo "CREATE USER '${WSREP_SST_USER}'@'localhost' IDENTIFIED BY '${WSREP_SST_PASSWORD}';" >> "$tempSqlFile"
+            echo "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '${WSREP_SST_USER}'@'localhost';" >> "$tempSqlFile"
+        fi
+        echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
+        
+        set -- "$@" --init-file="$tempSqlFile"
+    fi
+    
+    chown -R mysql:mysql "$DATADIR"
 fi
 
 exec "$@"
